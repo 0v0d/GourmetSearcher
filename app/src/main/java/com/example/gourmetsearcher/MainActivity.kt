ee6ec97b515d50897973ce94c2100c8473c710d9
0v0d <energyremtu@gmail.com>
2024-02-20 02:01:41 +0900

    MainActivityにtoolbarの追加とnavigationグラフの追加

--- app/src/main/java/com/example/gourmetsearcher/MainActivity.kt
+++ app/src/main/java/com/example/gourmetsearcher/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.gourmetsearcher
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.gourmetsearcher.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val hostFragment = binding.navHostFragment.getFragment<NavHostFragment>()
+        navController = hostFragment.navController
+
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
+
+    override fun onSupportNavigateUp(): Boolean = navController.navigateUp()
 }
