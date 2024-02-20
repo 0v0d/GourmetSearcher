22e661ef4b94ba1f46e77093ec9c065414a5f4d9
0v0d <energyremtu@gmail.com>
2024-02-20 23:05:13 +0900

    位置情報取得を取得する処理を追加

---
+++ app/src/main/java/com/example/gourmetsearcher/SearchLocationViewModel.kt
@@ -0,0 +1,48 @@
+package com.example.gourmetsearcher
+
+import android.content.Context
+import android.location.Location
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.OnCompleteListener
+
+class SearchLocationViewModel : ViewModel() {
+    private val _locationData = MutableLiveData<LastLocation>()
+    val locationData: LiveData<LastLocation> get() = _locationData
+
+    private val _errorState = MutableLiveData<Boolean>()
+    val errorState: LiveData<Boolean> get() = _errorState
+
+    fun getLocation(context: Context) {
+        try {
+            val fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
+            fusedLocationClient.lastLocation.addOnCompleteListener(locationCompleteListener)
+        } catch (e: SecurityException) {
+            handleLocationError()
+        }
+    }
+
+    private val locationCompleteListener = OnCompleteListener { task ->
+        try {
+            if (task.isSuccessful && task.result != null) {
+                handleLocationSuccess(task.result)
+            } else {
+                handleLocationError()
+            }
+        } catch (e: Exception) {
+            handleLocationError()
+        }
+    }
+
+    private fun handleLocationSuccess(location: Location) {
+        val locationData = LastLocation(location.latitude, location.longitude)
+        //val locationData = LastLocation(34.663706, 135.5182538)
+        _locationData.value = locationData
+    }
+
+    private fun handleLocationError() {
+        _errorState.value = true
+    }
+}
