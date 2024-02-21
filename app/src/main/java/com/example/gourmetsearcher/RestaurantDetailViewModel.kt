a4a4cb33d1d8c512f240a85e8eeda1393a7e1e19
energ <energyremtu@gmail.com>
2024-02-21 18:44:32 +0900

    レストラン詳細画面のViewModelを追加

---
+++ app/src/main/java/com/example/gourmetsearcher/RestaurantDetailViewModel.kt
@@ -0,0 +1,21 @@
+package com.example.gourmetsearcher
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import java.net.URLEncoder
+
+class RestaurantDetailViewModel : ViewModel(){
+    private val _url = MutableLiveData<String>()
+    val url: LiveData<String> get() = _url
+
+    fun openMap(address: String){
+        val searchAddress = URLEncoder.encode(address, "UTF-8")
+        val url = "geo:0:0?q=${searchAddress}"
+        openUrl(url)
+    }
+
+    fun openUrl(htmlUrl: String) {
+        _url.value = htmlUrl
+    }
+}
