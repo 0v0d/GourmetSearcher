1c88761aa3c0006fd0fd00eb44b85346795a1489
energ <energyremtu@gmail.com>
2024-02-21 18:43:54 +0900

    レストラン詳細画面の追加

--- app/src/main/java/com/example/gourmetsearcher/RestaurantDetailFragment.kt
+++ app/src/main/java/com/example/gourmetsearcher/RestaurantDetailFragment.kt
@@ -1,10 +1,41 @@
 package com.example.gourmetsearcher
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.net.toUri
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
+import com.example.gourmetsearcher.databinding.FragmentRestaurantDetailBinding
 
 class RestaurantDetailFragment : Fragment() {
+    private var _binding: FragmentRestaurantDetailBinding? = null
+    private val binding get() = _binding!!
+    private val args: RestaurantDetailFragmentArgs by navArgs()
+    private val viewModel: RestaurantDetailViewModel by viewModels()
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentRestaurantDetailBinding.inflate(inflater, container, false)
+        binding.restaurant = args.restaurantData
+
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+        viewModel.url.observe(viewLifecycleOwner) {
+            startActivity(Intent(Intent.ACTION_VIEW, it.toUri()))
+        }
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
 }
