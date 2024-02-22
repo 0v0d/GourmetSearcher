efd00be006a7ad3acdebb05b4dd3d765d1fac6d5
energ <energyremtu@gmail.com>
2024-02-22 19:42:07 +0900

    @AndroidEntryPointを追加

--- app/src/main/java/com/example/gourmetsearcher/MainActivity.kt
+++ app/src/main/java/com/example/gourmetsearcher/MainActivity.kt
@@ -7,7 +7,9 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.gourmetsearcher.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
     private lateinit var binding: ActivityMainBinding
