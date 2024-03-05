ea57b0493dd89906dad74b9ab9cc64eac588e7d7
energ <energyremtu@gmail.com>
2024-03-06 05:18:16 +0900

    明示的Intentを使用するように変更

--- app/src/main/java/com/example/gourmetsearcher/view/RestaurantDetailFragment.kt
+++ app/src/main/java/com/example/gourmetsearcher/view/RestaurantDetailFragment.kt
@@ -1,14 +1,16 @@
 package com.example.gourmetsearcher.view
 
+import android.content.ComponentName
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.net.toUri
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
+import com.example.gourmetsearcher.R
 import com.example.gourmetsearcher.databinding.FragmentRestaurantDetailBinding
 import com.example.gourmetsearcher.viewmodel.RestaurantDetailViewModel
 
@@ -28,10 +30,35 @@ class RestaurantDetailFragment : Fragment() {
         binding.viewModel = viewModel
         binding.lifecycleOwner = viewLifecycleOwner
         viewModel.url.observe(viewLifecycleOwner) {
-            startActivity(Intent(Intent.ACTION_VIEW, it.toUri()))
+            openWebBrowser(it)
         }
-
+        viewModel.searchAddress.observe(viewLifecycleOwner) {
+            openMap(it)
+        }
         return binding.root
+    }
+
+    private fun openWebBrowser(url: String) {
+        val intent = Intent(Intent.ACTION_VIEW, url.toUri())
+        intent.setComponent(
+            ComponentName(
+                "com.android.chrome",
+                "com.google.android.apps.chrome.Main"
+            )
+        )
+        startActivity(intent)
+    }
+
+    private fun openMap(address: String) {
+        val mapUrl = getString(R.string.map_url) + address
+        val intent = Intent(Intent.ACTION_VIEW, mapUrl.toUri())
+        intent.setComponent(
+            ComponentName(
+                "com.google.android.apps.maps",
+                "com.google.android.maps.MapsActivity"
+            )
+        )
+        startActivity(intent)
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/gourmetsearcher/viewmodel/RestaurantDetailViewModel.kt
+++ app/src/main/java/com/example/gourmetsearcher/viewmodel/RestaurantDetailViewModel.kt
@@ -5,14 +5,15 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import java.net.URLEncoder
 
-class RestaurantDetailViewModel : ViewModel(){
+class RestaurantDetailViewModel : ViewModel() {
     private val _url = MutableLiveData<String>()
     val url: LiveData<String> get() = _url
 
-    fun openMap(address: String){
-        val searchAddress = URLEncoder.encode(address, "UTF-8")
-        val url = "geo:0:0?q=${searchAddress}"
-        openUrl(url)
+    private val _searchAddress = MutableLiveData<String>()
+    val searchAddress: LiveData<String> get() = _searchAddress
+
+    fun openMap(address: String) {
+        _searchAddress.value = URLEncoder.encode(address, "UTF-8")
     }
 
     fun openUrl(htmlUrl: String) {
