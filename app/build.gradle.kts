008c1c4d4893d8f87af34cd8f34c2c26ae6306e9
0v0d <energyremtu@gmail.com>
2024-05-12 20:02:02 +0900

    build.gradleをkts化した

---
+++ app/build.gradle.kts
@@ -0,0 +1,99 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.androidx.navigation.safeargs)
+    //APIキーを隠すためのプラグイン
+    alias(libs.plugins.secrets.gradle.plugin)
+    alias(libs.plugins.kotlin.parcelize)
+    alias(libs.plugins.dagger.hilt.android)
+    alias(libs.plugins.kotlin.kapt)
+}
+
+android {
+    namespace = "com.example.gourmetsearcher"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.gourmetsearcher"
+        minSdk = 32
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isShrinkResources = true
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        buildConfig = true
+        dataBinding = true
+        viewBinding = true
+    }
+    hilt {
+        enableAggregatingTask = true
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.navigation.runtime.ktx)
+    //NavHostFragmentのために
+    implementation(libs.androidx.navigation.fragment.ktx)
+    //Toolbarのために
+    implementation(libs.androidx.navigation.ui.ktx)
+    //位置情報取得ライブラリ
+    implementation(libs.play.services.location)
+    //retrofit
+    implementation(libs.retrofit)
+    // MoshiのConverterを使うためのJsonパースライブラリ
+    implementation(libs.retrofit.converter.moshi)
+    // Jsonパーサライブラリ
+    implementation(libs.moshi.kotlin)
+    //画像表示ライブラリ
+    implementation(libs.picasso)
+
+
+    //Dagger-Hilt
+    implementation(libs.dagger.hilt.android)
+    kapt(libs.dagger.hilt.android.compiler)
+
+    // メモリリーク検出ライブラリ
+    debugImplementation(libs.leakcanary)
+
+    //MockitoJUnitRunner
+    testImplementation(libs.mockito.core)
+    testImplementation(libs.junit)
+    testImplementation(libs.dagger.hilt.android.testing)
+    kaptTest(libs.dagger.hilt.android.compiler)
+    testImplementation(libs.androidx.runner)
+    testImplementation(libs.androidx.core.testing)
+    testImplementation(libs.kotlinx.coroutines.test)
+
+    //Espresso
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(libs.androidx.uiautomator)
+}
+kapt {
+    // エラータイプの修正を有効化
+    correctErrorTypes = true
+}
---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.androidx.navigation.safeargs) apply false
+    alias(libs.plugins.kotlin.kapt) apply false
+    alias(libs.plugins.dagger.hilt.android) apply false
+    alias(libs.plugins.kotlin.parcelize) apply false
+    alias(libs.plugins.secrets.gradle.plugin) apply false
+}
+allprojects {
+    afterEvaluate {
+        tasks.withType<JavaCompile>().configureEach {
+            // 警告メッセージの設定
+            options.compilerArgs.addAll(listOf("-Xlint:unchecked", "-Xlint:deprecation"))
+        }
+    }
+}
