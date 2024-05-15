b0ab07fed3623709cfd5fba05e787db4f22dc5a7
0v0d <energyremtu@gmail.com>
2024-05-15 15:56:35 +0900

    テストを追加

---
+++ app/src/test/java/com/example/gourmetsearcher/repository/KeyWordHistoryRepositoryImplMockitoTest.kt
@@ -0,0 +1,44 @@
+package com.example.gourmetsearcher.repository
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito.verify
+import org.mockito.Mockito.`when`
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class KeyWordHistoryRepositoryImplTest {
+
+    @Mock
+    private lateinit var preferences: PreferencesManger
+
+    @InjectMocks
+    private lateinit var keyWordHistoryRepository: KeyWordHistoryRepositoryImpl
+    
+    @Test
+    fun testSaveHistoryItemWithNewEntry() {
+        val input = "keyword1"
+        keyWordHistoryRepository.saveHistoryItem(input)
+        verify(preferences).saveHistoryItem(input)
+    }
+
+    @Test
+    fun testGetHistoryList() {
+        val expectedHistoryList = listOf("keyword1", "keyword2", "keyword3")
+        `when`(preferences.getHistoryList()).thenReturn(expectedHistoryList)
+
+        val actualHistoryList = keyWordHistoryRepository.getHistoryList()
+
+        assertEquals(expectedHistoryList, actualHistoryList)
+        verify(preferences).getHistoryList()
+    }
+
+    @Test
+    fun testClearHistory() {
+        keyWordHistoryRepository.clearHistory()
+        verify(preferences).clearHistory()
+    }
+}
