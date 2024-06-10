1f5d61a154a38f515da2a55a65dd4127375ee0f8
0v0d <energyremtu@gmail.com>
2024-06-10 22:30:52 +0900

    UseCaseの追加

---
+++ app/src/main/java/com/example/gourmetsearcher/usecase/SaveKeyWordHistoryUseCase.kt
@@ -0,0 +1,18 @@
+package com.example.gourmetsearcher.usecase
+
+import com.example.gourmetsearcher.repository.KeyWordHistoryRepository
+import javax.inject.Inject
+
+/**
+ * 検索履歴を保存するUseCase
+ * @param repository KeyWordHistoryRepository
+ */
+class SaveKeyWordHistoryUseCase
+    @Inject
+    constructor(private val repository: KeyWordHistoryRepository) {
+        /**
+         * 検索履歴を保存する
+         * @param keyword 検索キーワード
+         */
+        operator fun invoke(keyword: String) = repository.saveHistoryItem(keyword)
+    }
