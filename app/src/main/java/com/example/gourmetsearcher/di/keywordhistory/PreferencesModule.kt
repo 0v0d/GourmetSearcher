ebf5bbf33257940f9eef8792fb4422378783055e
0v0d <energyremtu@gmail.com>
2024-06-27 12:20:11 +0900

    DataStoreを用いたPreferencesManagerの提供に変更

--- app/src/main/java/com/example/gourmetsearcher/di/keywordhistory/PreferencesModule.kt
+++ app/src/main/java/com/example/gourmetsearcher/di/keywordhistory/PreferencesModule.kt
@@ -1,6 +1,9 @@
 package com.example.gourmetsearcher.di.keywordhistory
 
 import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
 import com.example.gourmetsearcher.manager.PreferencesManager
 import dagger.Module
 import dagger.Provides
@@ -13,14 +16,25 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object PreferencesModule {
+    /** データストアを提供する */
+    private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "HistoryPrefs")
+
+    /**
+     * データストアを提供する
+     * @param context コンテキスト
+     * @return データストア
+     */
+    @Provides
+    fun provideDataStore(
+        @ApplicationContext context: Context,
+    ): DataStore<Preferences> = context.dataStore
+
     /**
      * プリファレンスマネージャを提供する
-     * @param context コンテキスト
+     * @param dataStore データストア
      * @return プリファレンスマネージャ
      */
     @Provides
     @Singleton
-    fun providePreferencesManger(
-        @ApplicationContext context: Context,
-    ): PreferencesManager = PreferencesManager(context)
+    fun providePreferencesManger(dataStore: DataStore<Preferences>): PreferencesManager = PreferencesManager(dataStore)
 }
